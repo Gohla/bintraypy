839a216c4483dec6a96f20d8ab1e1fc618c5d914
Gabriel Konat <gabrielkonat@gmail.com>
2016-09-28 17:31:22 +0200

    Improve code quality. Add method for creating a package.

--- bintraypy/bintray.py
+++ bintraypy/bintray.py
@@ -11,22 +11,44 @@ class Bintray(object):
     self.key = key
     self.url = url
 
-  def upload_generic(self, local_file_path, organization, repo, package, version, remote_file_path=None, publish=False,
-      override=False, explode=False):
+  def upload_generic(self, org, repo, package, version, local_file_path, remote_file_path=None, publish=False,
+      override=False):
     if not remote_file_path:
       remote_file_path = os.path.basename(local_file_path)
-    path = '{}/{}/{}/{}/{}'.format(organization, repo, package, version, remote_file_path)
-    url = '{}/content/{}?publish={}?override={}?explode={}'.format(self.url, path, _bool_to_int(publish),
-      _bool_to_int(override), _bool_to_int(explode))
+    path = 'content/{}/{}/{}/{}/{}'.format(org, repo, package, version, remote_file_path)
+    parameters = {}
+    if publish:
+      parameters['publish'] = '1'
+    if override:
+      parameters['override'] = '1'
     with open(local_file_path, "rb") as file:
       print('Uploading file {} to {}'.format(local_file_path, path))
-      if self.username and self.key:
-        response = requests.put(url, auth=(self.username, self.key), data=file)
-      else:
-        response = requests.put(url, data=file)
+      response = self.__create_request('put', path, params=parameters, data=file)
       if response.status_code != 201:
         raise Exception('Failed to upload file: {0}\n{1}'.format(response.status_code, response.text))
     print('Uploaded successfully')
+
+  def create_package(self, org, repo, name, licenses, vcs_url, description=None):
+    path = 'packages/{}/{}'.format(org, repo)
+    parameters = {
+      'name'    : name,
+      'licenses': licenses,
+      'vcs_url' : vcs_url
+    }
+    if description:
+      parameters['description'] = description
+    print('Creating package {} in {}'.format(name, path))
+    response = self.__create_request('post', path, params=parameters)
+    if response.status_code != 201:
+      raise Exception('Failed to create package: {0}\n{1}'.format(response.status_code, response.text))
+    print('Created package successfully')
+
+  def __create_request(self, method, path, **kwargs):
+    url = '{}/{}'.format(self.url, path)
+    if self.username and self.key:
+      return requests.request(method, url, auth=(self.username, self.key), **kwargs)
+    else:
+      return requests.request(method, url, **kwargs)
 
 
 def _bool_to_int(b):
